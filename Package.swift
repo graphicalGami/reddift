ea96e0f3944e476fe7e4559601806122489b760e
graphicalGami <9807195+graphicalGami@users.noreply.github.com>
2026-02-14 14:57:55 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 // swift-tools-version:5.5
 import PackageDescription
-
 let package = Package(
     name: "reddift",
     platforms: [
@@ -16,8 +15,8 @@ let package = Package(
     dependencies: [
         //.package(url: "https://github.com/sonsongithub/HTMLSpecialCharacters.git", from: "1.0.0"),
         //.package(url: "https://github.com/timpickles/MiniKeychain.git", branch: "master")
-		// xtool disables automatic package resolution, so specify rev until that is fixed
-		.package(url: "https://github.com/sonsongithub/HTMLSpecialCharacters.git", revision: "7797f4cca9f19c54436ad7470222130227cba98d"),
+        // xtool disables automatic package resolution, so specify rev until that is fixed
+        .package(url: "https://github.com/sonsongithub/HTMLSpecialCharacters.git", revision: "7797f4cca9f19c54436ad7470222130227cba98d"),
         .package(url: "https://github.com/timpickles/MiniKeychain.git", revision: "9ff7178f1f1369f2f4b2f72be97a0aa903ad5bc7")
     ],
     targets: [
@@ -28,7 +27,11 @@ let package = Package(
                 .product(name: "MiniKeychain", package: "MiniKeychain"),
             ],
             path: "framework",
-            sources: ["."]
+            exclude: [
+                "Info.plist",
+                "Model/generator.rb",
+                "Model/source.html"
+            ]
         )
     ]
 )
