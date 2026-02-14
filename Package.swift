8806ff48b2d813f3a38fbce918ce0b7b7b5a4514
graphicalGami <9807195+graphicalGami@users.noreply.github.com>
2026-02-14 11:39:59 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,8 +14,11 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/sonsongithub/HTMLSpecialCharacters.git", from: "1.0.0"),
-        .package(url: "https://github.com/timpickles/MiniKeychain.git", branch: "master")
+        //.package(url: "https://github.com/sonsongithub/HTMLSpecialCharacters.git", from: "1.0.0"),
+        //.package(url: "https://github.com/timpickles/MiniKeychain.git", branch: "master")
+		// xtool disables automatic package resolution, so specify rev until that is fixed
+		.package(url: "https://github.com/sonsongithub/HTMLSpecialCharacters.git", revision: "7797f4cca9f19c54436ad7470222130227cba98d"),
+        .package(url: "https://github.com/timpickles/MiniKeychain.git", revision: "9ff7178f1f1369f2f4b2f72be97a0aa903ad5bc7")
     ],
     targets: [
         .target(
